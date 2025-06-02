c5cc76b090e680af31c87991d1b6a923e6dabf1a
vijendra <bijendra7089660142@gmail.com>
2025-06-02 07:39:26 +0530

    Home page responsiveness done✅

--- Frontend/Spotify/src/Layout/MainLayout.tsx
+++ Frontend/Spotify/src/Layout/MainLayout.tsx
@@ -4,30 +4,50 @@ import LeftSidebar from "./components/LeftSidebar";
 import FriendsActivity from "./components/FriendsActivity";
 import AudioPlayer from "./components/AudioPlayer";
 import { PlaybackControls } from "./components/PlaybackControls";
+import { useEffect, useState } from "react";
 
-  
+
 
 const MainLayout = () => {
-    const isMobile = false;
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    }
+
+    checkMobile(); // Initial check
+    window.addEventListener('resize', checkMobile); // Listen for resize events
+    return () => {
+      window.removeEventListener('resize', checkMobile); // Cleanup listener on unmount
+    }
+  }, [isMobile])
+
   return (
     <div className="h-screen w-full flex flex-col bg-black text-white">
-      <ResizablePanelGroup direction="horizontal" className="h-screen flex overflow-hidden flex-1 p-4">
+      <ResizablePanelGroup direction="horizontal" className="h-screen flex overflow-hidden flex-1 p-4 pl-0">
         <AudioPlayer />
-        <ResizablePanel defaultSize={15} maxSize={20} minSize={isMobile ? 0 : 10}>
-            <LeftSidebar />
+        <ResizablePanel defaultSize={isMobile ? 20 : 10} maxSize={20} minSize={isMobile ? 0 : 10}>
+          <LeftSidebar />
         </ResizablePanel>
         <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
         <ResizablePanel defaultSize={isMobile ? 80 : 70} >
-            <div className="h-full overflow-y-auto hide-scrollbar">
-                <Outlet />
-            </div>
+          <div className="h-full overflow-y-auto hide-scrollbar">
+            <Outlet />
+          </div>
         </ResizablePanel>
-        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
-        <ResizablePanel defaultSize={15} minSize={0} maxSize={20}>
-            <FriendsActivity />
-        </ResizablePanel>
+        {
+          !isMobile && (
+            <>
+              <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+              <ResizablePanel defaultSize={20} minSize={0} maxSize={20} collapsedSize={0} >
+                <FriendsActivity />
+              </ResizablePanel>
+            </>
+          )
+        }
       </ResizablePanelGroup>
       <PlaybackControls />
     </div>
--- Frontend/Spotify/src/pages/Home/components/TrendingSection.tsx
+++ Frontend/Spotify/src/pages/Home/components/TrendingSection.tsx
@@ -1,4 +1,4 @@
-import TrendingSectionSkeleton from "@/components/skeletons/TrendingSectionSkeleton"
+import TrendingSectionSkeleton from "@/components/skeletons/TrendingSectionSkeleton";
 import { useMusicStore } from "@/Store/useMusicStore";
 import PlayButton from "../PlayButton";
 
@@ -6,42 +6,43 @@ const TrendingSection = () => {
     const { trendingSongs, isLoading, error } = useMusicStore();
     if (isLoading) return <TrendingSectionSkeleton />;
 
-    if(error) return (
-        <div className="text-red-500 text-center mt-4">
-            <p>Error loading trending songs: {error}</p>
-        </div>
-    )
+    if (error)
+        return (
+            <div className='text-red-500 text-center mt-4'>
+                <p>Error loading trending songs: {error}</p>
+            </div>
+        );
 
-  return (
-    <div className="mb-8">
-        <section className="max-w-7xl mx-auto">
-            <div className="flex items-center justify-between mb-4">
-                <h2 className="text-2xl font-bold text-white">Trending Now</h2>
-            </div>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
-                {trendingSongs.map((song) => (
-                    <div 
-                        key={song._id} 
-                        className="cursor-pointer group relative overflow-hidden rounded-xl bg-gradient-to-b from-neutral-800 to-neutral-900 p-4 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl"
-                    >
-                        <div className="aspect-square overflow-hidden rounded-lg mb-4">
-                            <img 
-                                src={song.imageUrl} 
-                                alt={song.title} 
-                                className="h-full w-full object-cover transition-transform duration-300 group-hover:scale-110" 
-                            />
+    return (
+        <div className='mb-8'>
+            <section className='max-w-7xl mx-auto'>
+                <div className='flex items-center justify-between mb-4'>
+                    <h2 className='text-2xl font-bold text-white'>Trending Now</h2>
+                </div>
+                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6'>
+                    {trendingSongs.map((song) => (
+                        <div
+                            key={song._id}
+                            className='cursor-pointer group relative overflow-hidden rounded-xl bg-gradient-to-b from-neutral-800 to-neutral-900 p-4 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl'>
+                            <div className='aspect-square overflow-hidden rounded-lg mb-4'>
+                                <img
+                                    src={song.imageUrl}
+                                    alt={song.title}
+                                    className='h-full w-full object-cover transition-transform duration-300 group-hover:scale-110'
+                                />
+                            </div>
+                            <h3 className='font-bold text-white truncate'>{song.title}</h3>
+                            <p className='text-gray-400 text-sm mt-1 truncate'>
+                                {song.artist}
+                            </p>
+                            <div className='absolute top-0 left-0 w-full h-full bg-black/40 opacity-0 transition-opacity duration-300 group-hover:opacity-100' />
+                            <PlayButton song={song} queue={trendingSongs} />
                         </div>
-                        <h3 className="font-bold text-white truncate">{song.title}</h3>
-                        <p className="text-gray-400 text-sm mt-1 truncate">{song.artist}</p>
-                        <div className="absolute top-0 left-0 w-full h-full bg-black/40 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
-                        <PlayButton song={song} queue={trendingSongs} />
-                        
-                    </div>
-                ))}
-            </div>
-        </section>
-    </div>
-  )
-}
+                    ))}
+                </div>
+            </section>
+        </div>
+    );
+};
 
 export default TrendingSection;
