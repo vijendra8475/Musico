6fca4b1366385ef00e3d26bfc7c90d3831f7b5b5
vijendra <bijendra7089660142@gmail.com>
2025-06-10 21:34:24 +0530

    bug fixes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -73,9 +73,9 @@ app.use('/api/stats', statsRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirName, "../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirName, "../frontend", "dist", "index.html"));
     });
 }
 
