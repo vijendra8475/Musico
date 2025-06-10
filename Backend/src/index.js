2329e0b2744272d008840467acc0fcfd1fa4d016
vijendra <bijendra7089660142@gmail.com>
2025-06-10 17:22:18 +0530

    Project Completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,7 +4,9 @@ import { clerkMiddleware } from '@clerk/express'
 import fileupload from 'express-fileupload';
 import path from 'path'
 import cors from 'cors'
+import fs from 'fs'
 import { createServer } from 'http';
+import cron from 'node-cron'
 
 import { initilizeSocket } from './lib/socket.js';
 
@@ -44,6 +46,24 @@ app.use(
         }
     }))
 
+
+// cron jobs
+const tempDir = path.join(process.cwd(), "temp");
+cron.schedule("0 * * * *", () => {
+	if (fs.existsSync(tempDir)) {
+		fs.readdir(tempDir, (err, files) => {
+			if (err) {
+				console.log("error", err);
+				return;
+			}
+			for (const file of files) {
+				fs.unlink(path.join(tempDir, file), (err) => {});
+			}
+		});
+	}
+});
+
+
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
@@ -53,10 +73,10 @@ app.use('/api/stats', statsRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-	});
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 
